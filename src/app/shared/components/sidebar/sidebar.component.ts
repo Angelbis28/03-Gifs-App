943544716601d07e5304570325fb60307e96bd08
Angelbis28 <angelbisfabian@gmail.com>
2023-07-15 16:58:31 -0300

    Leer del load Storage

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -10,8 +10,13 @@ export class SidebarComponent {
 
   // private gifsService
   constructor( private gifsService: GifsService ) {}
-  get tags() {
+
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+
+  searchTag( tag: string ){
+    this.gifsService.searchTag( tag );
   }
 
 
